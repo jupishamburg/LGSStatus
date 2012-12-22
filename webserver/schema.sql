de07b35f921d4fb53c1ce4ee803dbf00c318e599
Leonard Techel <mail@telelo.de>
2012-12-22 04:40:33 +0100

    webserver: sql schema added

---
+++ webserver/schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE clients (id Integer Primary Key Autoincrement, value Integer, time Integer);
+CREATE TABLE temperature (id Integer Primary Key Autoincrement, value Integer, time Integer);
+CREATE TABLE door (id Integer Primary Key Autoincrement, value Boolean, time Integer);
