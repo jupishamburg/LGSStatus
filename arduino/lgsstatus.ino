70871a8616961b96556f7f5e0f17e5a147720687
Leonard Techel <mail@telelo.de>
2012-12-22 04:38:57 +0100

    arduino script added

---
+++ arduino/lgsstatus.ino
@@ -0,0 +1,36 @@
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  if(Serial.available()>0){
+    Serial.print(analogRead(4)*0.004882812*100-273.15);
+    Serial.print(",");cd
+    Serial.print(analogRead(0));
+    Serial.println();
+    Serial.read();
+  }
+}void setup(){
+  Serial.begin(9600);
+}
+float temp = 0; int hell = 0; int i = 0;
+
+
+void loop(){
+  if(Serial.available()>0){
+    while(i < 10){
+      temp += analogRead(4)*0.004882812*100-273.15;
+      hell += analogRead(0);
+      i++;
+    }
+    temp /= 10;
+    hell /= 10;
+    
+    Serial.print(int(temp));
+    Serial.print(",");
+    Serial.print(hell);
+    Serial.println();
+    Serial.read();
+  }
+  
+}
