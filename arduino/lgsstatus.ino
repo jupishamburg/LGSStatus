e4d05a60be27cf35d4ab02b11738aa7c613b2526
sodaflo <derecht@googlemail.com>
2012-12-24 13:50:37 +0100

    Update arduino/lgsstatus.ino
    Filter für die Temperatur eingebaut

--- arduino/lgsstatus.ino
+++ arduino/lgsstatus.ino
@@ -2,24 +2,37 @@ void setup(){
   Serial.begin(9600);
 }
 
-float temp = 0; int hell = 0; int i = 0;
+float temp[] = {0, 0, 0, 0, 0}; int hell = 0; int i = 0; int n = 0; int l =0;
 
 
 void loop(){
+    
   if(Serial.available()>0){
+    if(n > 4)
+      {
+        n = 0;
+        l = 1;
+      }
     while(i < 10){
-      temp += analogRead(4)*0.004882812*100-273.15;
+      temp[n] += analogRead(4)*0.004882812*100-273.15;
       hell += analogRead(0);
       i++;
     }
-    temp /= 10;
+    temp[n] /= 10;
     hell /= 10;
-    
-    Serial.print(int(temp));
+    if(l == 1){
+    temp[5] = temp[0] + temp[1] + temp[2] + temp[3] + temp[4];
+    temp[5] /= 5;
+    }
+    else{
+      temp[5] = temp[n];
+    }
+    Serial.print(int(temp[5]));
     Serial.print(",");
     Serial.print(hell);
     Serial.println();
     Serial.read();
+    n++;
   }
   
 }
