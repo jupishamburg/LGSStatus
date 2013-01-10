08c9b5c6a7c2534b4f39ff9215f0f78a9cbd0981
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-01-10 20:09:32 +0100

    beautify

--- webserver/schema.sql
+++ webserver/schema.sql
@@ -1,3 +1,17 @@
-CREATE TABLE clients (id Integer Primary Key Autoincrement, value Integer, time Integer);
-CREATE TABLE temperature (id Integer Primary Key Autoincrement, value Integer, time Integer);
-CREATE TABLE door (id Integer Primary Key Autoincrement, value Boolean, time Integer);
+CREATE TABLE clients (
+  id Integer Primary Key Autoincrement,
+  value Integer,
+  time Integer
+);
+
+CREATE TABLE temperature (
+  id Integer Primary Key Autoincrement,
+  value Integer,
+  time Integer
+);
+
+CREATE TABLE door (
+  id Integer Primary Key Autoincrement,
+  value Boolean,
+  time Integer
+);
