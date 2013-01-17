f042199eeea3e543afbfc6ec62acc2d6bfab24b6
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-01-17 20:05:31 +0100

    Refactoring
    encapsulate JS
    not tested ^_^

---
+++ webserver/LGSStatus/templates/js/lgsStatePlotter.js
@@ -0,0 +1,47 @@
+(function lgsState($, document, window, lgsStateData) {
+	var day = [
+		{
+			label:'Geräte im Netz',
+			data:data.clients,
+			color:'#11644D'
+		},
+		{
+			label:'Temperatur in °C',
+			data:data.temperature,
+			color:'#F24E4E'
+		},
+		{
+			label:'Türstatus',
+			data:data.doorState,
+			color:'#F78145',
+			lines:{
+				fill:true
+			}
+		}
+	];
+
+	$(document).ready(function () {
+		var day_plot = $.plot($('#day'), day, {
+			xaxis:{
+				mode:'time',
+				ticks:10,
+				minTickSize:[1, 'hour'],
+				min:(new Date().getTime()) - 864e+5,
+				max:(new Date().getTime()),
+				timezone:'browser'
+			},
+			yaxis:{
+				tickSize:1
+			},
+			legend:{
+				position:'nw'
+			}
+		});
+
+		$(window).resize(function () {
+			day_plot.resize();
+			day_plot.setupGrid();
+			day_plot.draw();
+		});
+	});
+})(jQuery, document, window, lgsStateData);
