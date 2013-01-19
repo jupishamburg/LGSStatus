901b65e5fa236a646e622362b263834fc04594fa
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-01-19 01:06:48 +0100

    refactored code to avoid this drastic kind of nesting

--- webserver/LGSStatus/backend.py
+++ webserver/LGSStatus/backend.py
@@ -3,24 +3,24 @@ from LGSStatus import app, config, cur, db, helpers
 
 @app.route("/append/<type>")
 def insert(type):
-	# key check
-	if bottle.request.query.key in config["keys"]:
-		# type check
-		if type in config["types"]:
-			# value check
-			if bottle.request.query.value:
-				cur.execute("INSERT INTO {0} (value, time) VALUES (?, ?);".format(type), (
-					bottle.request.query.value,
-					int(time.time())
-				))
-				db.commit()
-				
-				# door tweet thingy
-				if type == "door":
-					helpers.doorTweet()
-				
-				return "1"
-			
-			bottle.abort(400, "you think nobody needs the value? then i can't agree with you.")
+	if not bottle.request.query.key in config["keys"]:
+		bottle.abort(403, "Security token not given")
+
+	if type not in config["types"]:
 		bottle.abort(400, "what about correct types?")
-	bottle.abort(403, "muhahaha penis")
+
+
+	if not bottle.request.query.value:
+		bottle.abort(400, "you think nobody needs the value? then i can't agree with you.")
+
+	cur.execute("INSERT INTO {0} (value, time) VALUES (?, ?);".format(type), (
+		bottle.request.query.value,
+		int(time.time())
+	))
+	db.commit()
+
+	# door tweet thingy
+	if type == "door":
+		helpers.doorTweet()
+
+	return "1"
