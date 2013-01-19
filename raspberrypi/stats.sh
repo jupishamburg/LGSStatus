e30ba20b1706289649d26d5e4f222438aa530c76
Leonard Techel <mail@telelo.de>
2013-01-19 16:39:55 +0100

    all the fixes

--- raspberrypi/stats.sh
+++ raspberrypi/stats.sh
@@ -6,9 +6,8 @@ TTY="/dev/ttyUSB0"
 NET="192.168.178.0/24"
 
 ## network clients
-
-networkClientsAmount = `nmap -sn ${NET} | grep 'Host is up' | wc -l`
-curl -d 'key=${KEY}\&value=${networkClientsAmount}' '${URL}/clients'
+networkClientsAmount=`nmap -sn ${NET} | grep 'Host is up' | wc -l`
+curl -d "key=${KEY}&value=${networkClientsAmount}" "${URL}/clients"
 
 ## temperature and door
 python2 ./temperature-door.py ${URL} ${KEY} ${TTY}
