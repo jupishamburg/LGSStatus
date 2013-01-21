d6f2fccb9afdf69cd5c8c53171f47c6dc21f7aa3
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-01-21 03:48:41 +0100

    Automagically convert the SASS file to CSS

--- webserver/LGSStatus/__init__.py
+++ webserver/LGSStatus/__init__.py
@@ -10,6 +10,10 @@ bottle.TEMPLATE_PATH.append("./LGSStatus/templates")
 
 twitter = twitterHandler.TwitterHandler(config, dbManager)
 
+bashCommand = "sass --watch ./LGSStatus/static/css/lgsstatus.sass:./LGSStatus/static/css/lgsstatus.css"
+import subprocess
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+
 @app.route("/static/<filepath:path>")
 def static(filepath):
 	return bottle.static_file(filepath, root="./LGSStatus/static")
