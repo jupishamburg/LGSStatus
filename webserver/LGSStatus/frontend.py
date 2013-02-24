9be0fd7cca0b1966bed73a13e36ca003ae4fb563
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-02-24 16:22:35 +0100

    bugfix

--- webserver/LGSStatus/frontend.py
+++ webserver/LGSStatus/frontend.py
@@ -1,19 +1,19 @@
 import bottle, datetime
-from LGSStatus import app, db_manager, watch
+from LGSStatus import app, db, watch
 
 @app.route("/")
 def index():
 	data_of_today = {
-		"clients": db_manager.get_type_values("clients", 300),
-		"temperature": db_manager.get_type_values("temperature", 300),
-		"door": db_manager.get_type_values("door", 300)
+		"clients": db.get_type_values("clients", 300),
+		"temperature": db.get_type_values("temperature", 300),
+		"door": db.get_type_values("door", 300)
 	}
 
 	return bottle.jinja2_template(
 		"index.html",
 		data_of_today = data_of_today,
-		is_door_open = db_manager.is_door_open(),
-		last_updated = datetime.datetime.fromtimestamp(db_manager.get_last_door_timestamp()).strftime("%d.%m.%Y %H:%M")
+		is_door_open = db.is_door_open(),
+		last_updated = datetime.datetime.fromtimestamp(db.get_last_door_timestamp()).strftime("%d.%m.%Y %H:%M")
 
 	)
 
