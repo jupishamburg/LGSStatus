dbd4a63f82192bf8e08d302006ae714c8e7f58a4
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-02-28 22:28:30 +0100

    hbg;l

--- raspberrypi/temperature-door.py
+++ raspberrypi/temperature-door.py
@@ -8,27 +8,18 @@ class Arduino(object):
 		self.status = None
 
 		try:
-			self.serial = serial.Serial(port=port)
-			self.serial.flushInput()
-			self.serial.flushOutput()
-
-			while True:
-				print(self.serial.readline()
+			self.serial = serial.Serial(port=port, 9600)
 		except:
 			"Failed to connect!"
 
-		if self.serial.write("k") is 1:
-			self.readStatus()
-
-	def readStatus(self):
-		self.status = self.serial.readline()
+		if self.serial.inWaiting():
+			self.status = self.serial.readline()
 
 	def getStatus(self):
 		return self.status
 
 	def getDoorState(self):
 		status = self.getStatus()
-		print status
 		status = status.replace("\r\n", "").split(",")
 		door_state = "1" if (int(status[1]) > 150) else "0"
 
@@ -37,7 +28,6 @@ class Arduino(object):
 	def getTemperature(self):
 		# dirty hack because of electrical problems
 		status = self.getStatus()
-		print status
 		status = status.replace("\r\n", "").split(",")
 		temperature = int(status[0]) - 12
 
@@ -46,16 +36,18 @@ class Arduino(object):
 class Poster(object):
 	def post_door_state(base_url, door_state, security_token):
 		params = {
-		'value': door_state,
-		'key': security_token
+			'value': door_state,
+			'key': security_token
 		}
+
 		requests.post(base_url + "/door", params)
 
 	def post_temperature(base_url, temperature, security_token):
 		params = {
-		'value': temperature,
-		'key': security_token
+			'value': temperature,
+			'key': security_token
 		}
+
 		requests.post(base_url + "/temperature", params)
 
 base_url = sys.argv[1]
@@ -65,6 +57,11 @@ port = sys.argv[3]
 arduino = Arduino(port)
 
 status = arduino.getStatus()
+
+print 'frak this:'
+print arduino.getStatus()
+print arduino.getDoorState()
+print arduino.getTemperature()
 
 #poster = Poster()
 #poster.post_door_state(base_url, arduino.getDoorState(), security_token)
