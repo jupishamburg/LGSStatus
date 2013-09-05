eca4cf8c8a73bb53b0218a0bbff536defebdf6ef
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2013-09-05 23:09:25 +0200

    fix

--- arduino/lgsstatus/lgsstatus.ino
+++ arduino/lgsstatus/lgsstatus.ino
@@ -14,7 +14,7 @@ float temp = .0;
 int lumen = 0;
 
 void loop() {
-    temp = analogRead(4) 0.00488759 * 100 - 273.15;
+    temp = analogRead(4) * 0.00488759 * 100 - 273.15;
     lumen = analogRead(0);
     Serial.print(temp);
     Serial.print("|");
